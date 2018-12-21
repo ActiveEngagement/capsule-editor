74e902289cac65fcca1dd5ddbd38b3ff1d9658c9
Justin Kimbrell <jkimbrell@actengage.com>
2018-12-21 16:44:18 -0700

    Initial commit

---
+++ src/SampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="message" v-html="message" data-test="1"/>
+</template>
+
+<script>
+export default {
+
+    name: 'sample-component',
+
+    props: {
+        message: {
+            type: String,
+            required: true
+        }
+    },
+
+    mounted() {
+        console.log('1111');
+    }
+
+};
+</script>
+
+<style lang="scss">
+.sample-component {
+    background: rgb(200, 200, 3);
+
+    .a {
+        background: red;
+    }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import SampleComponent from './SampleComponent';
+
+if(window && window.Vue) {
+    window.Vue.component('sample-component', SampleComponent);
+}
+
+export default SampleComponent;
