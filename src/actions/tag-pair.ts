9abbd4d0f47130711b0e78b435464744006667c9
Justin Kimbrell <jkimbrell@actengag.com>
2023-12-21 12:29:23 -0700

    fix: issues with tag-pair auto fix not working

--- src/actions/tag-pair.ts
+++ src/actions/tag-pair.ts
@@ -1,144 +1,37 @@
 import { syntaxTree } from '@codemirror/language';
-import { Action, } from '@codemirror/lint';
-import { EditorView } from '@codemirror/view';
-import { SyntaxNodeRef } from '@lezer/common';
-
-class Element {
-    public node: SyntaxNodeRef;
-    public parent?: Element;
-    // public closed: boolean;
-    public tagName?: string;
-    public name: string;
-    public from: number;
-    public to: number;
-    public isError: boolean;
-    public children: Element[];
-
-    constructor(view: EditorView, node: SyntaxNodeRef, parent?: Element, children: Element[] = []) {
-        this.node = node;
-        this.name = node.type.name;
-        this.parent = parent;
-        this.isError = node.type.name === '⚠';
-        this.children = children;
-        this.from = node.from;
-        this.to = node.to;
-        this.tagName = tagName(view, this.from, this.to);
-        // this.closed = node.type.name === 'Element' ? false : true;
-    }
-
-    fix(view: EditorView, at: Element) {
-        view.dispatch({
-            changes: {
-                from: at.from,
-                to: at.from,
-                insert: `</${this.tagName}>`
-            }
-        });
-
-        // this.closed = true;
-    }
-}
-
-function tagName(view: EditorView, from?: number, to?: number) {
-    return view.state.doc.sliceString(from ?? 0, to)
-        .toLowerCase()
-        .match(/^<\/?(?:\s+)?(\w+)/)?.[1];
-}
-
-// function changes(view: EditorView, from?: number, to?: number) {
-//     const elements = stack(view, from, to);
-
-//     const element = elements.filter(element => !element.closed).pop();
-
-//     if(!element) {
-//         return;
-//     }
-
-//     return {
-//         from: element.from,
-//         to: element.to,
-//         insert: `</${element.tagName}>`
-//     };
-// }
-
-function stack(view: EditorView, from?: number, to?: number): [Element|undefined, Element|undefined] {
-    let root: Element|undefined = undefined;
-    let current: Element|undefined = undefined;
-    
-    syntaxTree(view.state).iterate({
-        from,
-        to,
-        enter(node) {
-            const element = new Element(view, node, current);
-
-            if(!root) {
-                root = current;
-            }
-
-            if(current) {
-                current.children.push(element);
-            }
-            
-            current = element;            
-        },
-        leave() {
-            current = current?.parent;
-        }
-    });
-
-    return [root, root ? findFirstError(root) : undefined];
-}
-
-function findFirstError(el: Element): Element|undefined {
-    if(el.isError) {
-        return el;
-    }
-    
-    for(const child of el.children) {
-        const error = findFirstError(child);
-
-        if(error) {
-            return error;
-        }
-    }
-
-    return;
-}
-
-function findFirstUnclosedAncestor(el?: Element): Element | undefined {
-    if(!el) {
-        return;
-    }
-
-    if(el.name === 'Element') {
-        return el;
-    }
-
-    return findFirstUnclosedAncestor(el.parent);
-}
-
-function fix(view: EditorView, from: number, to: number): [Element|undefined, Element|undefined]  {
-    const [ root, error ] = stack(view, from, to);
-
-    if(!root || !error) {
-        return [undefined, undefined];
-    }
-        
-    const element = findFirstUnclosedAncestor(error);
-
-    if(!element) {
-        return [undefined, undefined];
-    }
-        
-    element.fix(view, error);
-
-    return stack(view, from, to);
-}
+import { Action } from '@codemirror/lint';
+import { SyntaxNode } from '@lezer/common';
 
 const actions: Action[] = [{
     name: 'Close Tag',
-    apply(view: EditorView, from: number, to: number) {
-        fix(view, from, to);
+    apply(view, from, to) {
+        const around: SyntaxNode = syntaxTree(view.state).resolveInner(to, -1);
+
+        let nearest: SyntaxNode|null = around;
+
+        while(nearest?.parent) {
+            if(nearest.name === 'Element' && !nearest.getChild('CloseTag')) {
+                break;
+            }
+
+            nearest = nearest.parent;
+        }
+            
+        const tagNode = nearest.firstChild?.getChild('TagName');
+        
+        if(!tagNode) {
+            return;
+        }
+
+        const tagName = view.state.doc.sliceString(
+            tagNode.from, Math.min(tagNode.to, view.state.doc.length)
+        );
+
+        view.dispatch({
+            changes: {
+                from, to, insert: `</${tagName}>`
+            }
+        });
     }
 }
 ];
