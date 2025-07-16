8f1fe9a75eacf2fb9dee94eb62e674236bb5a15b
Justin Kimbrell <jkimbrell@actengag.com>
2025-07-16 10:31:05 -0600

    chore: added demo app

---
+++ demo/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Editor from '../src/Editor.vue';
+
+// @ts-ignore
+import input from './input.html?raw';
+
+const content = ref<string>(input);
+</script>
+
+<template>
+    <Editor
+        :content="content"
+        class="h-screen" />
+</template>
