e27690ce9ab06bd2ee1d471079c3fde072ab5bad
Justin Kimbrell <jkimbrell@actengag.com>
2026-07-07 11:24:48 -0600

    chore: added more examples for lint failures for demo/debugging

--- demo/App.vue
+++ demo/App.vue
@@ -3,13 +3,11 @@ import { ref } from 'vue';
 import Editor from '../src/Editor.vue';
 
 // @ts-ignore
-import input from './freemarker-syntax.html?raw';
+import input from './no-start-tag.html?raw';
 
 const content = ref<string>(input);
 </script>
 
 <template>
-    <Editor
-        :content="content"
-        class="h-screen" />
+    <Editor :content="content" class="h-screen" />
 </template>
